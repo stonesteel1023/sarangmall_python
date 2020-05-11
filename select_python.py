10290db1e68c8dde066dcd0bf9dc51bc68788357
Michael Choi <stonesteel1023@gmail.com>
2020-05-11 11:41:01 +0900

    Update select_python.py

--- select_python.py
+++ select_python.py
@@ -36,21 +36,21 @@ conn.close()
 
 
 ### 현재 발생하고 있는 에러
-> D:\python_workspace\sarangmall_python\select_python.py:10: DeprecationWarning: Using or importing the ABCs from 'collections' instead of from 'collections.abc' is deprecated since Python 3.3, and in 3.9 it will stop working
-  import pymssql
-Traceback (most recent call last):
-  File "src\pymssql.pyx", line 448, in pymssql.Cursor.execute
-  File "src\_mssql.pyx", line 1064, in _mssql.MSSQLConnection.execute_query
-  File "src\_mssql.pyx", line 1095, in _mssql.MSSQLConnection.execute_query
-  File "src\_mssql.pyx", line 1228, in _mssql.MSSQLConnection.format_and_run_query
-  File "src\_mssql.pyx", line 1639, in _mssql.check_cancel_and_raise
-  File "src\_mssql.pyx", line 1683, in _mssql.maybe_raise_MSSQLDatabaseException
-_mssql.MSSQLDatabaseException: (207, b"Invalid column name '2020-05-11'.DB-Lib error message 20018, severity 16:\nGeneral SQL Server error: Check messages from the SQL Server\nDB-Lib error message 20018, severity 16:\nGeneral SQL Server error: Check messages from the SQL Server\n")
+# **\sarangmall_python\select_python.py:10: DeprecationWarning: Using or importing the ABCs from 'collections' instead of from 'collections.abc' is deprecated since Python 3.3, and in 3.9 it will stop working
+#  import pymssql
+# Traceback (most recent call last):
+#  File "src\pymssql.pyx", line 448, in pymssql.Cursor.execute
+#  File "src\_mssql.pyx", line 1064, in _mssql.MSSQLConnection.execute_query
+#  File "src\_mssql.pyx", line 1095, in _mssql.MSSQLConnection.execute_query
+#  File "src\_mssql.pyx", line 1228, in _mssql.MSSQLConnection.format_and_run_query
+#  File "src\_mssql.pyx", line 1639, in _mssql.check_cancel_and_raise
+#  File "src\_mssql.pyx", line 1683, in _mssql.maybe_raise_MSSQLDatabaseException
+# _mssql.MSSQLDatabaseException: (207, b"Invalid column name '2020-05-11'.DB-Lib error message 20018, severity 16:\nGeneral SQL Server error: Check messages from the SQL Server\nDB-Lib error message 20018, severity 16:\nGeneral SQL Server error: Check messages from the SQL Server\n")
 
-During handling of the above exception, another exception occurred:
+# During handling of the above exception, another exception occurred:
 
-Traceback (most recent call last):
-  File "D:\python_workspace\sarangmall_python\select_python.py", line 23, in <module>
-    cur.execute('SELECT SUBSTRING(orderno, 16, 5), ' \
-  File "src\pymssql.pyx", line 465, in pymssql.Cursor.execute
-pymssql.ProgrammingError: (207, b"Invalid column name '2020-05-11'.DB-Lib error message 20018, severity 16:\nGeneral SQL Server error: Check messages from the SQL Server\nDB-Lib error message 20018, severity 16:\nGeneral SQL Server error: Check messages from the SQL Server\n")
+# Traceback (most recent call last):
+#  File "D:\python_workspace\sarangmall_python\select_python.py", line 23, in <module>
+#    cur.execute('SELECT SUBSTRING(orderno, 16, 5), ' \
+#  File "src\pymssql.pyx", line 465, in pymssql.Cursor.execute
+# pymssql.ProgrammingError: (207, b"Invalid column name '2020-05-11'.DB-Lib error message 20018, severity 16:\nGeneral SQL Server error: Check messages from the SQL Server\nDB-Lib error message 20018, severity 16:\nGeneral SQL Server error: Check messages from the SQL Server\n")
